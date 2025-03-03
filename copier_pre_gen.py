8650bbd8678f0b5cd4501cd3c338a32b3a472c10
bozman2021 <bozman2021@users.noreply.github.com>
2025-03-03 20:19:17 +0100

    Create copier_pre_gen.py

---
+++ copier_pre_gen.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+def generate_password(length=16):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(characters) for _ in range(length))
+
+def hook(context):
+    context['postgress_password'] = generate_password()
